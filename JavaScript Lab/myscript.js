bc7e30b88e6cd6268f123d7e1e14235d77b903bd
taylorhamblin <thamblin@students.mchenry.edu>
2017-03-01 16:38:21 -0600

    Uploading JavaScript

---
+++ JavaScript Lab/myscript.js
@@ -0,0 +1,23 @@
+function change01(){
+ document.getElementById("large").src="Images/Cat1.jpg";
+}
+
+function change02(){
+    document.getElementById("large").src="Images/Cat2.jpg";
+}
+
+function change03(){
+    document.getElementById("large").src="Images/Cat3.jpg";
+}
+
+function change04(){
+    document.getElementById("large").src="Images/Cat4.jpg";
+}
+
+function change05(){
+    document.getElementById("large").src="Images/Cat5.jpg";
+}
+
+function change06(){
+    document.getElementById("large").src="Images/Cat6.jpg";
+}
